948fcec14a718fc4bc62d07afecd4267109fa00e
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2022-11-30 17:21:39 +0330

    add chart

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBagShopping , faFileInvoice } from '@fortawesome/free-solid-svg-icons'
 import styled from "styled-components"
 import styles from './dash.module.scss'
+import { PieChart } from 'react-minimal-pie-chart';
+
 
 
 const Container = styled.div`
@@ -41,7 +43,14 @@ function Dashboard () {
                 })}
             </div>
             <div className={styles.chart}>
-                
+                <PieChart
+                style={{ width:'300px' }}
+                    data={[
+                        { title: 'One', value: 10, color: '#3ABAF5' },
+                        { title: 'Two', value: 15, color: '#C13C37' },
+                        { title: 'Three', value: 20, color: '#3A69E6' },
+                    ]}
+                />
             </div>
         </Container>
     )
