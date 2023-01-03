9707abb2980ef28cdd66fdf3ad674f1df3d24f15
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-01-03 17:01:42 +0330

    end

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBagShopping , faFileInvoice } from '@fortawesome/free-solid-svg-icons'
+import { faFileInvoiceDollar , faFileInvoice , faWallet } from '@fortawesome/free-solid-svg-icons'
 import styled from "styled-components"
 import styles from './dash.module.scss'
 import PirChart from "../chart/pirChart"
@@ -28,7 +28,14 @@ function Dashboard () {
 
     const data = [
         {id:1 , name:'فاکتور ها' , int:'12' , link:'/manage-invoice' , icon:faFileInvoice},
-        {id:2 , name:'درخواست فاکتور رسمی' , int:'23' , link:'/request-invoice' , icon:faBagShopping},
+        {id:2 , name:'فاکتور رسمی' , int:'23' , link:'/request-invoice' , icon:faFileInvoice},
+        
+    ]
+
+    const datas = [
+        {id:1 , name:'فاکتور ها' , int:'12' , link:'/manage-invoice' , icon:faFileInvoice},
+        {id:2 , name:'فاکتور رسمی' , int:'23' , link:'/request-invoice' , icon:faFileInvoiceDollar},
+        {id:3 , name:'کیف پول' , int:'10000000' , link:null , icon:faWallet},
         
     ]
 
@@ -43,13 +50,14 @@ function Dashboard () {
     return(
         <Container>
             <div className={styles.row}>
-                {data.map((i , index)=> {
+                {datas.map((i , index)=> {
                     return(
                         <div key={index} className={styles.col6}>
                             <NavLink to={i.link}>
                                 <div className={styles.card}>
                                     <FontAwesomeIcon icon={i.icon}/>
-                                    <p>{i.name} <span>{i.int}</span></p>
+                                    <p>{i.name}</p>
+                                    <p>{i.int}</p>
                                 </div>
                             </NavLink>
                         </div>
