875fa9a6c2bce19c83eebab1732bace25efac0bb
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-01-08 17:52:40 +0330

    sa

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -4,7 +4,6 @@ import WizardForm from 'react-wizard-form'
 import StepOne from "./step-one"
 import StepTwo from "./step-two"
 import StepThere from "./step-there"
-import axios from "axios"
 
 
 
@@ -15,29 +14,10 @@ padding:25px 15px 0px 15px;
 
 function Login (){
 
-    const [apiLogin , setApiLogin] = useState({})
-
-    useEffect(()=> {
-        const dataLogin = async() => {
-            const Response = await axios.get(`https://reqres.in/api/users?page=2`)
-            setApiLogin(Response.data.data)
-        }
-
-        dataLogin();
-    },[])
-
     // title page
     useEffect(()=> {
         document.title = 'ورود به حساب کاربری'
     })
-
-    // data login
-    // const request = [
-    //     {id:1 , mobile:'09199954356' , code:'2341'},
-    //     {id:2 , mobile:'09379026444' , code:'1234'},
-    // ]
-
-    // console.log(apiLogin)
 
     const [mobileData , setMobileData] = useState()
 
